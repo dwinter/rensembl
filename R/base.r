9349271f2341c9b3355e0b55d150a178e955cd93
David Winter <david.winter@gmail.com>
2015-01-30 10:21:48 -0700

    gene tree format selector

--- R/base.r
+++ R/base.r
@@ -1,9 +1,15 @@
+#Extra functions
+#
+#Save the lat complete request in the the environment as a last, 
+# use to check retry etc.
+# Timout function so not GET/POST
+
 
 base_url <- function() "http://rest.ensembl.org"
 
 ensembl_GET  <- function(end_point, base=base_url(), headers=c(), ...){
     req  <- httr::GET(base_url(), path=end_point, add_headers=headers, ...)
-#    httr::content(req)
+    ensembl_check(req)
     req
 }
 
@@ -26,11 +32,24 @@ lookup_id <- function(id){
     res
 }
 
+gene_tree_header <- function(tree_format){
+    h <- switch(tree_format, "nh"      =  "text/x-nh", 
+                            "nexus"   =  "text/x-nh", 
+                            "pyloxml" =  "text/x-phyloxml+xml",
+                            "json"    =   "application/json", 
+                 stop(paste("Unkown format for gene tree:", tree_format)))
+    httr::accept(h)    
+}
+
+
 gene_tree <- function(spp, sym){
     end <- paste("genetree/member/symbol", spp, sym, sep="/")  
-    res <- ensembl_GET(end, 
-                       httr::add_headers("accept"="text/x-nh"),
+    sp_t <- ensembl_GET(end, 
+                       httr::accept("accept"="text/x-nh"),
                        query=list(nh_format="species"))
-    ensembl_check(res)
-    res
+    b_t <- ensembl_GET(end, 
+                       httr::add_headers("accept"="text/x-nh"))
+    tr <- read.tree(b_t)
+    tr$tip.label <- ape::read.tree(sp_t)$tip.label
+    tr
 }
