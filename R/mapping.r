11005cc435aac2035190e526ce6e0032ae3f94c5
David Winter <david.winter@gmail.com>
2015-03-20 12:06:25 -0700

    include ID in gene mapping fxn

--- R/mapping.r
+++ R/mapping.r
@@ -1,8 +1,8 @@
 #'Convert  coordinates between gene
 #'@export
 map_genomic_coordinates <- function(id, type, region, format='json'){
-    header = ensembl_header(format)
-    end <- paste("map", type, region, sep="/")
+    header = ensembl_header(format, c("json"))
+    end <- paste("map", type, id, region, sep="/")
     req <- ensembl_GET(end,header=header)
     httr::content(req)
 }
