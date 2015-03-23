1e7f4ec40523c6b93b87de12fa2f3df3f2629fd1
David Winter <david.winter@gmail.com>
2015-03-23 10:26:18 -0700

    liftover like fxn map_species_builds

--- R/mapping.r
+++ R/mapping.r
@@ -1,4 +1,4 @@
-#'Convert  coordinates between gene
+#'Convert  coordinates between gene and genome
 #'@export
 map_genomic_coordinates <- function(id, type, region, format='json'){
     header = ensembl_header(format, c("json"))
@@ -7,3 +7,14 @@ map_genomic_coordinates <- function(id, type, region, format='json'){
     httr::content(req)
 }
 
+#'
+#'@export
+map_species_builds <- function(species, region, asm_one, asm_two, 
+                               coord_system="chromosome", format='json'){
+    header = ensembl_header(format, c("json", "xml"))
+    end <- paste("map", species, asm_one, region, asm_two, sep="/")
+    print(end)
+    q <- list(coord_system=coord_system)
+    req <- ensembl_GET(end, header=header, body=q)
+    httr::content(req)
+}
