3ecca66f945209a4e9000681055e77236cb2150e
David Winter <david.winter@gmail.com>
2015-04-19 11:26:44 -0700

    variation functions work

--- R/variation.r
+++ R/variation.r
@@ -1,18 +1,19 @@
+#'@export
 variation_id <- function(ids, species= "hsap", genotypes=FALSE, phenotypes=FALSE,
                          pops=FALSE, population_genotypes=FALSE, req_format="json"){
     header <- ensembl_header(req_format, c("json", "xml"))
-    query <- ensembl
-    if(length(ids) > 1){
-        vars_post(end)
-    }
+    query <- ensembl_body(match.call(), exclude=c("ids", "species"))
+    protocol_fxn <- if(length(ids) > 1) vars_post else vars_get
+    req <- protocol_fxn(ids, species, query=query, header=header)
+    httr::content(req)
+}
 
-
-vars_post <- function(ids, species= "hsap", header, query)
+vars_post <- function(ids, species= "hsap", query, header){
     end <- paste("variation", species, sep="/")
-    ensembl_POST(end, body=ids, query=query, header)
+    ensembl_POST(end, body=list(ids=ids), query=query, header)
 }
 
 vars_get <- function(ids, species= "hsap", header, query){
-    end <- paste("variation", species, ids sep="/")
+    end <- paste("variation", species, ids, sep="/")
     ensembl_GET(end, query=query, header)
 }
