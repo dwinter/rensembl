dd6ac47cfb6bdb3ae1c78550638c3968c555c678
David Winter <david.winter@gmail.com>
2015-04-24 11:43:57 -0700

    bug in varition finder, was not looking upstream

--- R/caroline.r
+++ R/caroline.r
@@ -1,6 +1,6 @@
 #'@export
 vars_from_symbol <- function(symbol, flanking=0, format="json"){
     gene <- lookup_symbol(symbol)
-    region <- paste0(gene$seq_region_name, ":", gene$start - flanking, "-", gene$end - flanking)
+    region <- paste0(gene$seq_region_name, ":", gene$start - flanking, "-", gene$end + flanking)
     overlap(region=region, feature="variation", format=format)
 }
